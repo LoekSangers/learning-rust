4dbb7cfba5adbd4ea3b34a173549d0c7b8dacb39
Loek Sangers <9132841+LoekSangers@users.noreply.github.com>
2022-01-24 20:00:53 +0000

    initial website :)

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # learning-rust
 Repository in which I plan to do projects to learn to work with rust.
 
-Projects found on:
-https://github.com/practical-tutorials/project-based-learning#rust
+working with guides from hyper.rs
---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
+use hyper::service::{make_service_fn, service_fn};
+
+use futures::TryStreamExt;
+
+
+async fn hello(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        },
+
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        },
+
+        (&Method::POST, "/echo/uppercase") => {
+            // This is actually a new `futures::Stream`...
+            let mapping = req
+                .into_body()
+                .map_ok(|chunk| {
+                    chunk.iter()
+                        .map(|byte| byte.to_ascii_uppercase())
+                        .collect::<Vec<u8>>()
+                });
+        
+            // Use `Body::wrap_stream` to convert it to a `Body`...
+            *response.body_mut() = Body::wrap_stream(mapping);
+        },
+
+        (&Method::POST, "/echo/reverse") => {
+            // Await the full body to be concatenated into a single `Bytes`...
+            let full_body = hyper::body::to_bytes(req.into_body()).await?;
+        
+            // Iterate the full body in reverse order and collect into a new Vec.
+            let reversed = full_body.iter()
+                .rev()
+                .cloned()
+                .collect::<Vec<u8>>();
+        
+            *response.body_mut() = reversed.into();
+        },
+
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        },
+    };
+
+    Ok(response)
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+#[tokio::main]
+pub async fn main(){
+    pretty_env_logger::init();
+
+    // For every connection, we must make a `Service` to handle all
+    // incoming HTTP requests on said connection.
+    let service = make_service_fn(|_conn| {
+        // This is the `Service` that will handle the connection.
+        // `service_fn` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        async { Ok::<_, hyper::Error>(service_fn(hello)) }
+    });
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let server = Server::bind(&addr).serve(service);
+
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    // Run this server for... forever!
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
+}
