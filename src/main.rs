840a9416baf0894bffe0a2b55a5838e506b9fb6b
Loek Sangers <9132841+LoekSangers@users.noreply.github.com>
2022-12-04 15:00:56 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,46 @@
-use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 use futures::TryStreamExt;
-
 
 async fn hello(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
-
         (&Method::GET, "/") => {
             *response.body_mut() = Body::from("Try POSTing data to /echo");
-        },
+        }
 
         (&Method::POST, "/echo") => {
             *response.body_mut() = req.into_body();
-        },
+        }
 
         (&Method::POST, "/echo/uppercase") => {
             // This is actually a new `futures::Stream`...
-            let mapping = req
-                .into_body()
-                .map_ok(|chunk| {
-                    chunk.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Vec<u8>>()
-                });
-        
+            let mapping = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+
             // Use `Body::wrap_stream` to convert it to a `Body`...
             *response.body_mut() = Body::wrap_stream(mapping);
-        },
+        }
 
         (&Method::POST, "/echo/reverse") => {
             // Await the full body to be concatenated into a single `Bytes`...
             let full_body = hyper::body::to_bytes(req.into_body()).await?;
-        
+
             // Iterate the full body in reverse order and collect into a new Vec.
-            let reversed = full_body.iter()
-                .rev()
-                .cloned()
-                .collect::<Vec<u8>>();
-        
+            let reversed = full_body.iter().rev().cloned().collect::<Vec<u8>>();
+
             *response.body_mut() = reversed.into();
-        },
+        }
 
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-        },
+        }
     };
 
     Ok(response)
@@ -60,7 +54,7 @@ async fn shutdown_signal() {
 }
 
 #[tokio::main]
-pub async fn main(){
+pub async fn main() {
     pretty_env_logger::init();
 
     // For every connection, we must make a `Service` to handle all
